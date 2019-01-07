5eff46c6bf41ae45c9543b0533831dd787d5db3c
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-07 16:40:09 +0100

    modifies styling in tailwind.src.css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 
 const Footer = () => {
-        return (
-            <div class="border-t border-smoke px-8 py-4">
-                <div class="flex justify-center text-grey">
-                        Made with React
-                </div>
+    return (
+        <div class="border-t border-smoke px-8 py-4">
+            <div class="flex justify-center text-grey">
+                    Made with React
             </div>
-        )
-
+        </div>
+    )
 }
 
 export default Footer
