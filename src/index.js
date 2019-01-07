043fdc75db1dc71f9cb76ad3f731a9e71930c86e
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-07 19:16:38 +0100

    adds routes for about me and projects

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import Header from "./Header"
 import Footer from "./Footer"
 import './css/tailwind.css'
 import Projects from "./Projects"
-import About from "./About";
-import { BrowserRouter } from 'react-router-dom'
+import About from "./About"
+import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -16,7 +17,11 @@ const App = () => {
             <div className="page-content">
                 <Header />
                 <div className="content-wrapper">
-                    <Hello />
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                    </Switch>
                 </div>
                 <Footer />
             </div >
